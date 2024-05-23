9d8821e9cabc239815260c1acb10b43bc5a5beb0
zeusgolohor <zeusgolohor@gmail.com>
2024-05-23 04:25:41 +0000

    Create a class LRUCache that inherits from BaseCaching and is a caching system:
    You must use self.cache_data - dictionary from the parent class BaseCaching
    You can overload def __init__(self): but don’t forget to call the parent init: super().__init__()
    def put(self, key, item):
    Must assign to the dictionary self.cache_data the item value for the key key.
    If key or item is None, this method should not do anything.
    If the number of items in self.cache_data is higher that BaseCaching.MAX_ITEMS:
    you must discard the least recently used item (LRU algorithm)
    you must print DISCARD: with the key discarded and following by a new line
    def get(self, key):
    Must return the value in self.cache_data linked to key.
    If key is None or if the key doesn’t exist in self.cache_data, return None.

---
+++ 0x01-caching/3-lru_cache.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""
+A  class LRUCache that inherits from BaseCaching and is a caching system.
+"""
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """
+    A class LRUCache that inherits from BaseCaching and is a caching system.
+    """
+
+    def __init__(self):
+        """
+        A method to initialize the class.
+        """
+        super().__init__()
+        self.key_times = {}
+
+    def put(self, key, item):
+        """
+        A method to add an item to the cache.
+        """
+        if key is not None and item is not None:
+            if len(self.cache_data) >= self.MAX_ITEMS:
+                # Find the key with the minimum value in self.key_times (least recently used)
+                discarded_key = min(self.key_times, key=self.key_times.get)
+                del self.cache_data[discarded_key]
+                del self.key_times[discarded_key]
+
+            self.cache_data[key] = item
+            self.key_times[key] = 0  # Reset usage count
+
+
+    def get(self, key):
+        """
+        A method used to get a record from the cache.
+        """
+        if key is not None:
+            if key in self.cache_data:
+               self.key_times[key] += 1
+               return self.cache_data.get(key)
+        else:
+            return None
