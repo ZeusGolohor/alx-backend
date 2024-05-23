7e800362a4eeee1c179cbbb76597969b45b13a38
zeusgolohor <zeusgolohor@gmail.com>
2024-05-23 04:43:17 +0000

    Create a class MRUCache that inherits from BaseCaching and is a caching system:
    You must use self.cache_data - dictionary from the parent class BaseCaching
    You can overload def __init__(self): but don’t forget to call the parent init: super().__init__()
    def put(self, key, item):
    Must assign to the dictionary self.cache_data the item value for the key key.
    If key or item is None, this method should not do anything.
    If the number of items in self.cache_data is higher that BaseCaching.MAX_ITEMS:
    you must discard the most recently used item (MRU algorithm)
    you must print DISCARD: with the key discarded and following by a new line
    def get(self, key):
    Must return the value in self.cache_data linked to key.
    If key is None or if the key doesn’t exist in self.cache_data, return None.

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -2,7 +2,46 @@
 """
 A  class LRUCache that inherits from BaseCaching and is a caching system.
 """
+from collections import OrderedDict
 BaseCaching = __import__('base_caching').BaseCaching
 
 
+class MRUCache(BaseCaching):
+    """
+    A class MRUCache that inherits from BaseCaching and is a caching system.
+    """
+    def __init__(self):
+        """
+        A method to add an item to the cache.
+        """
+        super().__init__()
+        self.cache_data = OrderedDict()
 
+    def put(self, key, item):
+        """
+        A method to add an item to the cache.
+        """
+        if key is None or item is None:
+            return
+
+        if key not in self.cache_data:
+            if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
+                mru_key = next(iter(self.cache_data))
+                print("DISCARD:", mru_key)
+                del self.cache_data[mru_key]
+
+            self.cache_data[key] = item
+        else:
+            del self.cache_data[key]
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """
+        A method to add an item to the cache.
+        """
+        if key is not None and key in self.cache_data:
+            value = self.cache_data[key]
+            del self.cache_data[key]
+            self.cache_data[key] = value
+            return value
+        return None
