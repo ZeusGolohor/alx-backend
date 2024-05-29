9924d4a13f1e101e19e4c5fb58a6de4ccb2704cb
zeusgolohor <zeusgolohor@gmail.com>
2024-05-28 19:13:00 -0700

    t Creating a user login system is outside the scope of this project. To emulate a similar behavior, copy the following user table in 5-app.py. This will mock a database user table. Logging in will be mocked by passing login_as URL parameter containing the user ID to log in as.
    Define a get_user function that returns a user dictionary or None if the ID cannot be found or if login_as was not passed.
    
    Define a before_request function and use the app.before_request decorator to make it be executed before all other functions. before_request should use get_user to find a user if any, and set it as a global on flask.g.user.
    
    In your HTML template, if a user is logged in, in a paragraph tag, display a welcome message otherwise display a default message as shown in the table below.

---
+++ 0x02-i18n/5-app.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python3
+"""
+A basic flask app set up.
+"""
+from flask import Flask, render_template, g
+from flask_babel import Babel
+from flask import g, request
+
+
+class Config():
+    """
+    A class to configure the application locale.
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+# Fake Database
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+@babel.localeselector
+def get_locale():
+    """
+    A method to determine the best locale for a user using
+    the application.
+    """
+    # To detect if the incoming request contains locale
+    # argument and ifs value is a supported locale
+    if 'locale' in request.args:
+        locale = request.args.get('locale')
+        if locale in app.config['LANGUAGES']:
+            return locale
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+def get_user(user_id):
+    """
+    Used to get a user from the fake database if it exist.
+    """
+    return users.get(user_id)
+
+
+@app.before_request
+def before_request():
+    """
+    A method to run before any request is made.
+    """
+    user_id = int(request.args.get('login_as', 0))
+    g.user = get_user(user_id) if user_id else None
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """
+    A method to define the index if the application.
+    A method to determine the welcome message.
+    """
+    if g.user:
+        message = _('logged_in_as', username=g.user['name'])
+    else:
+        message = _('not_logged_in')
+    return render_template('3-index.html', message=message)
+
+
+if __name__ == '__main__':
+    app.run()
