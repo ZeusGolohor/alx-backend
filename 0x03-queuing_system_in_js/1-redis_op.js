25eaebaafecd3bd4bc0f313801d500930d114a45
zeusgolohor <zeusgolohor@gmail.com>
2024-06-27 00:41:17 +0000

    In a file 1-redis_op.js, copy the code you previously wrote (0-redis_client.js).
    Add two functions:
    
    setNewSchool:
    It accepts two arguments schoolName, and value.
    It should set in Redis the value for the key schoolName
    It should display a confirmation message using redis.print
    displaySchoolValue:
    It accepts one argument schoolName.
    It should log to the console the value for the key passed as argument
    At the end of the file, call:
    
    displaySchoolValue('Holberton');
    setNewSchool('HolbertonSanFrancisco', '100');
    displaySchoolValue('HolbertonSanFrancisco');
    Requirements:
    
    Use callbacks for any of the operation, we will look at async operations later

--- 0x03-queuing_system_in_js/1-redis_op.js
+++ 0x03-queuing_system_in_js/1-redis_op.js
@@ -1,4 +1,4 @@
-import redis from 'redis';
+import { redis, print } from 'redis';
 
 const client = redis.createClient();
 
@@ -9,3 +9,16 @@ client.on('connect', () => {
 client.on('error', (err) => {
   console.log(`Redis client not connected to the server: ${err.message}`);
 });
+
+const setNewSchool = (schoolName, value) => {
+  client.SET(schoolName, value, print);
+});
+
+const displaySchoolValue = (schoolName) => {
+  client.GET(schoolName, (_err, reply) => {
+    console.log(reply);
+  });
+};
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
